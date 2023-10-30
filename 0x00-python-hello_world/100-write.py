259ea1257f201c41d1661224ed10821930dd9408
Ruky8821 <rumena78@gmail.com>
2023-10-30 07:39:06 -0700

    100

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -3,9 +3,9 @@ mport sys
 
 str = "and that piece of art is useful - Dora Korpar, 2015-10-19"
 
-#print to stderr
+# print to stderr
 sys.stderr.write(str + "\n")
 
-#exit status 
+# exit status
 e_status = 1
 sys.exit(exit_status)
