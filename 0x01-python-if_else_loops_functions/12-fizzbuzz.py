4f2fee5d1c6894b8c45a7187fe2ca38ad8460dfa
Ruky8821 <rumena78@gmail.com>
2023-10-31 12:42:11 -0700

    t12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 for i in range(1, 100):
-    number = "FizzBuzz" if i % 3 == 0 and i % 5 == 0 
-    else "Fizz"  if i % 3 == 0 else "Buzz" if i % 5 == 0
+    number = "FizzBuzz" if i % 3 == 0 and i % 5 == 0
+    else "Fizz" if i % 3 == 0 else "Buzz" if i % 5 == 0
     else "{].format(i)
-    print(number, end=" ")
-
-    
+    print(number, end=" ")    
