3ac98ed5a80a775d625742d15b98e5e3dfa559e5
Ruky8821 <rumena78@gmail.com>
2023-10-31 07:02:51 -0700

    t9

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    numstr = str(abs(number))
+    numarr = []
+
+    for items in numstr:
+        numarr.append(int(items))
+
+    for x in range(0, len(numarr)):
+       # y = numarr[x]
+        if x == (len(numarr)-1):
+            print("{}".format(numarr[x]), end='')
