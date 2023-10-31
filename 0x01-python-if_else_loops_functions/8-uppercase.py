b54d4518a34626c5704f8422688cf093c1ee8b6c
Ruky8821 <rumena78@gmail.com>
2023-10-31 06:58:08 -0700

    8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,9 +2,8 @@
 def uppercase(str):
     upp = ""
     for lett in str:
-        if 97 <= ord(lett)  <= 122:
+        if 97 <= ord(lett) <= 122:
             upp = upp + chr(ord(lett) - 32)
         else:
             upp = upp + lett
     print("{}".format(upp))
-
