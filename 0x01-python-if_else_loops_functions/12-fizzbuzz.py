34686cc02a20fe1f7040158a1132385f2e8c4252
Ruky8821 <rumena78@gmail.com>
2023-10-31 12:54:31 -0700

    tt12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,6 +2,5 @@
 
 for i in range(1, 100):
     number = "FizzBuzz" if i % 3 == 0 and i % 5 == 0
-    else "Fizz" if i % 3 == 0 else "Buzz" if i % 5 == 0
-    else "{}".format(i)
-    print("{}".format(number), end=" ")
+    else "Fizz" if i % 3 == 0 else "Buzz" if i % 5 == 0 else "{}".format(    i)
+    print(number, end=" ")
