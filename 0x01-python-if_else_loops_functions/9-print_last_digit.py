dca8e8fe60abc425a365931331f6a57a0acc59c3
Ruky8821 <rumena78@gmail.com>
2023-10-31 07:49:14 -0700

    t9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,15 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    numstr = str(abs(number))
-    numarr = []
-
-    for items in numstr:
-        numarr.append(int(items))
-
-    for x in range(0, len(numarr)):
-        y = numarr[x]
-        z = (len(numarr)-1)
-        if number >= 0:
-                 print("{}".format(z))
-        else:
-                 print("-{}".format(z))
+    
+    z = int(abs(number)) % 10 
+    print("{}".format(z), end="")
