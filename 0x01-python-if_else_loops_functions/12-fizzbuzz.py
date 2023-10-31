30ae2b277c81334554ede15e50332cee7c361436
Ruky8821 <rumena78@gmail.com>
2023-10-31 13:11:57 -0700

    t12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-
-for i in range(1, 100):
-    number = "FizzBuzz" if i % 3 == 0 and i % 5 == 0
-    else "Fizz" if i % 3 == 0 else "Buzz" if i % 5 == 0 else "{}".format(    i)
-    print(number, end=" ")
+def fizzbuzz():
+    for i in range(1, 101):
+        number = "FizzBuzz" if i % 3 == 0 and i % 5 == 0 else "Fizz"\
+            if i % 3 == 0 else "Buzz" if i % 5 == 0 else "{}".format(i)
+        print(number, end=" ")
