bdb65623ba2c256ca6d93dd2f75cf55a0798264b
Ruky8821 <rumena78@gmail.com>
2023-10-31 05:04:48 -0700

    t3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 n = ""
 for j in range(97, 123):
-    if chr(j) == 'e' or j == 'q':
+    if chr(j) == 'e' ohar(j) == 'q':
         continue
     n += chr(j)
 print("{}".format(n), end='')
