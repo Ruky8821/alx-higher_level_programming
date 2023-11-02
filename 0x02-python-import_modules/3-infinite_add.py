6ad36f2448fd765645b55894e697cdd0638ce8c8
Ruky8821 <rumena78@gmail.com>
2023-11-02 02:49:40 -0700

    t3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-
-    if len(sys.argv) - 1 == 0:
-        print("{} arguments.".format(len(sys.argv) - 1))
-    elif len(sys.argv) - 1 == 1:
-        print("{} argument:".format(len(sys.argv) - 1))
-    else:
-        print("{} arguments:".format(len(sys.argv) - 1))
-    for x in range(len(sys.argv) - 1):
-        print("{}: {}".format(x + 1, sys.argv[x + 1]))
+    sum = 0
+    x = sys.argv[1:]
+    for i in x:
+        sum = sum + int(i)
+    print("{}".format(sum))
