a9590544a774a350d006a426901338df2a2899b3
Ruky8821 <rumena78@gmail.com>
2023-11-06 02:56:56 -0800

    t4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
 
-    if idx < 0 and idx > len(my_list):
+    if idx < 0:
+        return my_list
+    elif idx > (len(my_list) - 1):
         return my_list
     else:
         mod_list = my_list[:]
