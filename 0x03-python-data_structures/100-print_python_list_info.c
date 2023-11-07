71601acd0b699d80480d654f1b3d3c87b539b89e
Ruky8821 <rumena78@gmail.com>
2023-11-07 13:28:57 -0800

    t100

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,5 +1,8 @@
 #include <python.h>
-
+/**
+ * print_python_list_info - prints py info
+ * @z: varisbl
+ */
 void print_python_list_info(PyObject *z)
 {
 	int x, i;
@@ -10,12 +13,12 @@ void print_python_list_info(PyObject *z)
 	alc = ((PyListObject *)z)->allocated;
 	printf("[*] size of the Python Lis = %d \n", x);
 	printf("[*] Allocated = %d \n", i);
-	
+
 	while (y  < size)
 	{
 		printf("Element %d: ", y);
 		ob = PyList_GetItem(z, y);
-		printf("%s\n", PyTYPE(ob)->ob_type);	
+		printf("%s\n", PyTYPE(ob)->ob_type);
 		y++;
 	}
 }
