de25f9afa6e326b01b33c92a322609a8094f6206
Ruky8821 <rumena78@gmail.com>
2023-11-08 04:52:44 -0800

    t1

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new = matrix
+
+    for x in new:
+        print("[", end='')
+        for y, z in enumerate(x):
+            print("{:d}".format(z*z), end='')
+            if y < len(x) - 1:
+                print(", ", end='')
+        print("]")
